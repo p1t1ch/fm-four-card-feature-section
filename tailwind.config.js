37a45ec5a8081d941f42b6dfbfc5029f56c88caa
p1t1ch <belobeev.kirill@gmail.com>
2021-01-24 14:21:12 +0300

    add dynamic classnames to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    options: {
+      safelist: {
+        standard: [/^border-primary-/],
+      },
+    },
+  },
   theme: {
     colors: {
       primary: {
